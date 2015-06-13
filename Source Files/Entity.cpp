f1dea7b43dc2f00704b4ed4a9da6286596022a63
yaziid <cantinanv@hotmail.com>
2015-06-13 20:50:42 +0100

    Update Entity.cpp

--- Source Files/Entity.cpp
+++ Source Files/Entity.cpp
@@ -1,8 +1,27 @@
-#include "Header Files/Entity.h"
+#include "Entity.h"
 
-Entity::Entity()
+Entity::Entity(Type type)
 {
-
+    if(type == Player)
+    {
+        if(!_texture.loadFromFile("Player.png"))
+            throw std::runtime_error("Unable to load texture: Player.png");
+    }
+    else if(type == PNJ)
+    {
+        if(!_texture.loadFromFile("PNJ.png"))
+            throw std::runtime_error("Unable to load texture: PNJ.png");
+    }
+    else if(type == Mob)
+    {
+        if(!_texture.loadFromFile("Mob.png"))
+            throw std::runtime_error("Unable to load texture: Mob.png");
+    }
+    else
+    {
+        if(!_texture.loadFromFile("ERROR.png"))
+            throw std::runtime_error("Unable to load texture: ERROR.png");
+    }
 }
 
 sf::Vector2f Entity::getVelocity()
