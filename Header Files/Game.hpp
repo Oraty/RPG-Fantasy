dbe63c8852ce4e79cdc1170c3930ac544fd5e2eb
dalyl-zero <alidalyl@yahoo.fr>
2015-06-13 15:10:05 +0100

    Defined Game.hpp

---
+++ Header Files/Game.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+sf::Time const TimePerFrame = sf::seconds(1.f / 60.f);
+
+class Game
+{
+public :
+	Game();
+	void run();
+
+private :
+	void processEvents();
+	void update(sf::Time deltaTime);
+	void render();
+
+private :
+	sf::RenderWindow _window;
+};
