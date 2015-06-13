03b14712b3825ecb2a80868ee4efdc0934ac6145
yaziid <cantinanv@hotmail.com>
2015-06-13 20:01:43 +0100

    Create Entity.cpp

---
+++ Source Files/Entity.cpp
@@ -0,0 +1,32 @@
+#include "Header files/Entity.h"
+
+Entity::Entity()
+{
+
+}
+
+sf::Vector2f Entity::getVelocity()
+{
+    return _velocity;
+}
+
+sf::Vector2f Entity::getPosition()
+{
+    return _position;
+}
+
+void Entity::Velocity(sf::Vector2f velocity)
+{
+    _velocity = velocity;
+}
+
+void Entity::Position(sf::Vector2f position)
+{
+    _position = position;
+}
+
+void Entity::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+
+}
+
