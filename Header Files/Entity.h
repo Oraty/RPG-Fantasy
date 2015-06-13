e5997b754262da382351072b0222c37163059844
yaziid <cantinanv@hotmail.com>
2015-06-13 19:42:06 +0100

    Update Entity.h

--- Header Files/Entity.h
+++ Header Files/Entity.h
@@ -1,17 +1,19 @@
 #ifndef ENTITY_H_INCLUDED
 #define ENTITY_H_INCLUDED
 
+#include <SFML/Graphics.hpp>
+#include <string>
+
 class Entity
 {
 
     public:
-        bool colission();
+        Entity();
 
     private:
-        float _speed;
-        std::string name;
-        float velocity;
-
+        vector2f _velocity;
+        vector2f _position;
+        std::string _name
 };
 
 #endif // ENTITY_H_INCLUDED
